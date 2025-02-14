14b47fa7da158b2c7c7b95f12d281a25e089ccb6
Sven Urbach <urbachsven@gmail.com>
2025-02-14 11:06:19 +0100

    add title, fix logo

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="flex justify-between mb-10 h-28">
+    <div class="flex justify-between mb-10 h-12">
         <Logo />
+        <div>CineRatings</div>
         <Menu />
     </div>
     <div class="mb-10 mx-4">
--- app/components/Logo.vue
+++ app/components/Logo.vue
@@ -1,7 +1,4 @@
 <template>
-    <div class="w">
-        <img src="~/assets/images/cr-logo.png" alt="Logo" />
-      <!-- <slot /> -->
-    </div>
-  </template>
+    <img src="~/assets/images/cr-logo.png" alt="Logo" />
+</template>
   
