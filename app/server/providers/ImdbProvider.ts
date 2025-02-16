45ff411f9100a4324b685554303baace04174298
Sven Urbach <urbachsven@gmail.com>
2025-02-16 16:39:12 +0100

    add provider interface

---
+++ app/server/providers/ImdbProvider.ts
@@ -0,0 +1,6 @@
+// Hier sollen die Daten eines bestimmten Films beim Provider IMDB abgefragt und zurückgegeben werden.
+import { IProvider } from './IProvider';
+
+export class ImdbProvider implements IProvider {
+
+}
---
+++ app/server/providers/TmdbProvider.ts
@@ -0,0 +1,6 @@
+// Hier sollen die Daten eines bestimmten Films beim Provider TMDB abgefragt und zurückgegeben werden.
+import { IProvider } from './IProvider';
+
+export class TmdbProvider implements IProvider {
+
+}
