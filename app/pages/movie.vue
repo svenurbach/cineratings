77714fe053bb5837e09c7dec41a22796f01b4744
Sven Urbach <urbachsven@gmail.com>
2025-03-11 18:25:05 +0100

    remove debug

--- app/pages/movie.vue
+++ app/pages/movie.vue
@@ -14,27 +14,25 @@ onMounted(() => {
     <button @click="$router.back()">Zurück</button>
     <div class="my-2 p-2 border border-solid border-black">
       <h3>Filmdaten</h3>
-      <div class="flex flex-col p-2">
-        <SingleMovieDetail :label="'Titel'" :detail="movie.title" />
-        <SingleMovieDetail :label="'Jahr der Veröffentlichung'" :detail="movie.releaseDate" />
-        <SingleMovieDetail :label="'IMDb-ID'" :detail="movie.imdbId" />
+      <div class="flex">
+        <div>
+            <img :src="movie.posterUrl || 'images/poster-placeholder.jpg'" :alt="movie.title" class="w-20">
+        </div>
+        <div class="p-2">
+          <SingleMovieDetail :label="'Titel'" :detail="movie.title" />
+          <SingleMovieDetail :label="'Jahr der Veröffentlichung'" :detail="movie.releaseDate" />
+          <SingleMovieDetail :label="'IMDb-ID'" :detail="movie.imdbId" />
+        </div>
       </div>
     </div>
 
     <div class="my-2 p-2 border border-solid border-black">
-      <h3 class="text-lg font-bold">Anbieter</h3>
+      <h3>Anbieter</h3>
       <ProviderBox
         v-for="provider in movie.providers" 
         :key="provider.providerId" 
         :data="provider"
         class="p-2 border-b" />
-    </div>
-
-    <div class="my-2 p-2 border border-solid border-black">
-      <h3 class="text-lg font-bold">Alle Daten / Debug</h3>
-      <div class="p-2 border-b overflow-hidden">
-        {{ movie }}
-      </div>
     </div>
 
   </section>
