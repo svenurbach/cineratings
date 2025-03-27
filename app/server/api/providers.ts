4bd34df3f93aacadc691f300ea8190a57a526dee
Sven Urbach <urbachsven@gmail.com>
2025-03-27 16:58:27 +0100

    refactor getAllprovider

--- app/server/api/providers.ts
+++
@@ -1,7 +0,0 @@
-// Route welche sich von ProviderRegistry die verfügbaren Provider holt
-import ProviderService from '../../services/ProviderService';
-
-export default defineEventHandler((_event) => {
-    const providers = ProviderService.getAllProvidersFromFactory();
-    return providers;
- });
