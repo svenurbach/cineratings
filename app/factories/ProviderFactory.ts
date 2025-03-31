bcccf52e7ee2a064d77c26709e535f554fe08fd3
Sven Urbach <urbachsven@gmail.com>
2025-03-31 15:35:55 +0200

    remove allprovider

--- app/factories/ProviderFactory.ts
+++ app/factories/ProviderFactory.ts
@@ -1,56 +1,41 @@
-// Bekommt von der View/LocalStorage die ausgewählten Provider und gibt diese als Instanz zurück.
-// TODO: Vielleicht sollte die ProviderFactory die Providerklassen in einem property als lsite speichern und das objekt könnte dann immer abgerufen werden. damit man nicht x mal die klasse instanzieren muss.
-
+/**
+ * Factory für die Erstellung von MovieRatingProvider-Instanzen.
+ */
 import type { MovieRatingProvider } from "../interfaces/MovieRatingProvider";
-// TODO: Alle verfügbaren provider dynamisch importieren lassen
 import OmdbProvider from "../providers/OmdbProvider";
 import TmdbProvider from "../providers/TmdbProvider";
-import ImdbProvider from "../providers/ImdbProvider";
-import MetacriticProvider from "../providers/MetacriticProvider";
-import RottenTomatoesProvider from "../providers/RottentomatoesProvider";
+// import ImdbProvider from "../providers/ImdbProvider";
+// import MetacriticProvider from "../providers/MetacriticProvider";
+// import RottenTomatoesProvider from "../providers/RottentomatoesProvider";
 
 const providerMap: Record<string, new () => MovieRatingProvider> = {
   tmdb: TmdbProvider,
   omdb: OmdbProvider,
-  imdb: ImdbProvider,
-  metacritic: MetacriticProvider,
-  rottentomatoes: RottenTomatoesProvider
+  // imdb: ImdbProvider,
+  // metacritic: MetacriticProvider,
+  // rottentomatoes: RottenTomatoesProvider
 };
 
 export default class ProviderFactory {
-  // static getProvider(providerName: string) {
-  //   const providers: { [key: string]: Provider } = {
-  //     tmdb: new TmdbProvider(),
-  //     omdb: new OmdbProvider()
-  //   };
 
-  //   return providers[providerName] || null;
-  // }
-
-  static createProviders(providerNames: string[]): MovieRatingProvider[] {
+  /**
+   * Erstellt MovieRatingProvider-Instanzen anhand der übergebenen Provider-Namen.
+   * @param providerNames
+   * @returns MovieRatingProviders
+   */
+  public createProviders(providerNames: string[]): MovieRatingProvider[] {
     const providers: MovieRatingProvider[] = [];
 
     providerNames.forEach(provider => {
       const ProviderClass = providerMap[provider];
       if (!ProviderClass) {
-        throw new Error(`Unbekannter Provider-Typ: ${provider}`);
+        throw new Error(`Unbekannter Provider: ${provider}`);
       }
       providers.push(new ProviderClass());
     });
-    console.log("createProviders", providers);
 
-    return providers;
-  }
+    console.log("ProviderFactory > createProviders:", providers);
 
-  // getBaseProvider() {
-  //   const providers = this.createAllProvidersFromList();
-  //   const baseProvider = providers.find(provider => typeof(provider) === 'baseProvider');
-  //   return providerMap['omdb'];
-  // }
-
-  createAllProvidersFromList(): MovieRatingProvider[] {
-    const providers = Object.values(providerMap).map(ProviderClass => new ProviderClass());
-    console.log("createAllProviders", providers);
     return providers;
   }
 
