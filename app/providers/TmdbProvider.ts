83a25a87c92efd9b95dd6fdb1edd1f6bef1cca65
Sven Urbach <urbachsven@gmail.com>
2025-04-07 14:15:42 +0200

    image url

--- app/providers/TmdbProvider.ts
+++ app/providers/TmdbProvider.ts
@@ -8,7 +8,7 @@ export default class TmdbProvider implements MovieRatingProvider {
     readonly name = 'The Movie Database (TMDB)';
     readonly homepageUrl = "https://www.themoviedb.org/";
     readonly logoUrl = 'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg';
-    readonly posterBaseURlSmall = 'https://image.tmdb.org/t/p/w300/';
+    readonly posterBaseURlSmall = 'https://image.tmdb.org/t/p/w342/';
     readonly posterBaseURlMedium = 'https://image.tmdb.org/t/p/w500/';
 
     async searchMovie(query: string): Promise<MovieMetadata[]> {
