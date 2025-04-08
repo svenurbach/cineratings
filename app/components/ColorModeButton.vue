52e8b06f0ea520f926932c08bb8236c378f1f3ac
Sven Urbach <urbachsven@gmail.com>
2025-04-08 19:47:43 +0200

    colo mode button

---
+++ app/components/ColorModeButton.vue
@@ -0,0 +1,32 @@
+<script setup>
+const colorMode = useColorMode()
+
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+</script>
+
+<template>
+  <h2>Erscheinungsbild</h2>
+  <p>Wechsel zwischem hellem und dunklem Erscheinungsbild</p>
+  <ClientOnly v-if="!colorMode?.forced">
+    <UButton
+      :icon="isDark ? 'i-heroicons-sun' : 'i-heroicons-moon'"
+      color="neutral"
+      variant="outline"
+      size="xl"
+      class="w-min"
+      @click="isDark = !isDark"
+    >
+      {{ isDark ? 'Hell' : 'Dunkel' }}
+    </UButton>
+    <template #fallback>
+      <div class="size-8" />
+    </template>
+  </ClientOnly>
+</template>
