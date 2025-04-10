da01bc7efb723eb7a9f9f7551bb310d92f6ee390
Sven Urbach <urbachsven@gmail.com>
2025-04-10 12:10:16 +0200

    remove singleton

--- app/plugins/dataService.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtPlugin } from '#app';
-import DataService from '~/services/DataService';
-
-export default defineNuxtPlugin((nuxtApp) => {
-    // TODO: Abhängigkeiten übergeben. Z.B. ProviderFactory
-    // DataService als Singleton
-    const dataService = new DataService();
-    nuxtApp.provide('dataService', dataService);
-});
