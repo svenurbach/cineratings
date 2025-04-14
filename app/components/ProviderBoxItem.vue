5774a46ebdd4e84c80dac6959aa2e240e9fce301
Sven Urbach <urbachsven@gmail.com>
2025-04-14 11:23:37 +0200

    size, tooltip

--- app/components/ProviderBoxItem.vue
+++ app/components/ProviderBoxItem.vue
@@ -20,19 +20,20 @@ defineProps<{
 			:title="`Zur Webseite von ${data.name}`"
 			target="_blank"
 		>
-			<img class="size-8 sm:size-14" :src="data.logoUrl" loading="lazy" :alt="`Logo von ${data.name}`" >
+			<div class="text-(--ui-text) text-sm">{{ data.name }}</div>
 		</ULink>
 	</template>
+	<img class="size-6 sm:size-12" :src="data.logoUrl" loading="lazy" :alt="`Logo von ${data.name}`" >
     <template #footer>
-		<div class="flex flex-col items-center text-(--ui-text)">
-			<div v-if="data.primaryRating" class="font-bold text-lg">
+		<div class="flex flex-col text-center text-(--ui-text)">
+			<div v-if="data.primaryRating" class="font-bold text-lg"  title="Bewertung">
 				{{ data.primaryRating }}
 			</div>
-			<div v-if="data.userRating" class="font-bold text-lg">
+			<div v-if="data.userRating" class="font-bold text-lg" title="Nutzerbewertung">
 				{{ data.userRating.toFixed(1) }}
 			</div>
-			<div v-if="data.userVotes" class="text-sm">
-				{{ data.userVotes }} Votes
+			<div v-if="data.userVotes" class="text-sm" title="Anzahl an Nutzerbewertungen">
+				{{ data.userVotes }}
 			</div>
 		</div>
     </template>
